fbd770041800b3718764910a899dfda8cb51dc0e
Supro26 <suprojitmallick2006@gmail.com>
2025-11-27 16:45:35 +0530

    all file handeling codes

---
+++ FileHandeling/printfile.java
@@ -0,0 +1,28 @@
+// package FileHandeling;
+import java.io.*;
+import java.util.*;
+public class printfile {
+    public static void main(String[] args) {
+        try {
+            FileReader fr=new FileReader("/home/bokuto/Documents/test.txt");
+            int ch;
+            while((ch = fr.read()) != -1)
+            {
+                if((char)ch == '\n')
+                    System.out.println((char)ch);
+                else
+                    System.out.print((char)ch);
+            }
+
+        } 
+        catch (FileNotFoundException e) {
+        // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
